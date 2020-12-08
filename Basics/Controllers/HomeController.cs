70d063ff7738981dc64ce2bca93ed2764dc38fe4
bishwash369 <bishwashparajuli10@gmail.com>
2020-12-09 00:37:54 +0545

    Initial Commit

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@gmail.com"),
+                new Claim("Gramdma.Says", "Good boy"),
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob again"),
+                new Claim("Driving License", "A & B"),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "grandma identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+             
+            var userPrincipal = new ClaimsPrincipal(new[] {grandmaIdentity, licenseIdentity});
+
+            HttpContext.SignInAsync(userPrincipal);
+            return RedirectToAction("Index");
+        }
+    }
+}
